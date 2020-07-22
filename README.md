c0f479cf7699e5c252c033848805dbe046c48583
PredaaA <46051820+PredaaA@users.noreply.github.com>
2020-07-23 00:33:35 +0200

    *boom*

--- README.md
+++
@@ -1,3 +0,0 @@
-# aikaterna-cogs ~ Predä fork
-
-Welcome on this repo, which is mainly used to maintain the fork of otherbot [aikaterna's](https://github.com/aikaterna/aikaterna-cogs) cog.
--- otherbot/__init__.py
+++ otherbot/__init__.py
@@ -1,7 +1,14 @@
-from .otherbot import Otherbot
+from redbot.core.errors import CogLoadError
 
 
 async def setup(bot):
-    cog = Otherbot(bot)
-    await cog.generate_cache()
-    bot.add_cog(cog)
+    raise CogLoadError(
+        "This fork of otherbot has been deprecated as it as been merged to aikaterna's repository.\n\n"
+        "Steps to migrate (repo_name is the name of repo you've set when adding this repo):\n"
+        "1. `[p]cog uninstall otherbot`\n"
+        "2. `[p]repo delete repo_name`\n"
+        "3. `[p]repo add aikaterna https://github.com/aikaterna/aikaterna-cogs`\n"
+        "4. `[p]cog install aikaterna otherbot`\n"
+        "5. `[p]load otherbot`\n\n"
+        "**Your data will __not__ be lost by moving to this repo.**"
+    )
